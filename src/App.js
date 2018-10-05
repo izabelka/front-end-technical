f5fb5c4c2a89e9793e51d18f45a645f34d8e07cc
izabelka <i.konstanciak@gmail.com>
2018-10-04 18:14:18 -0700

    even more styling - make header keys look like buttons

--- src/App.js
+++ src/App.js
@@ -71,6 +71,8 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
 `;
 
 const Table = styled.div`
@@ -83,14 +85,17 @@ const HeaderRow = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: #fafbfc;
-  padding: 20px 0 20px 14px;
-  border-bottom: 1px solid #e1e4e8;
+  padding: 10px;
+  color: #14aaf5;
 `;
 
 const Key = styled.span`
   width: ${({ address }) => address ? 150 : 100}px;
   text-transform: capitalize;
+  padding: 10px;
+  border: 1px solid #14aaf5;
+  border-radius: 4px;
+  cursor: pointer;
 `;
 
 const DataWrapper = styled.div`
