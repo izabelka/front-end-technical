a1565a6d964fd505667ecbf30161e0097f5cc1da
izabelka <i.konstanciak@gmail.com>
2018-10-05 17:17:17 -0700

    Updates

---
+++ precache-manifest.29d32d9e67e8c79016285811ac52e9c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bfc54a3143d673e1a0d",
+    "url": "/front-end-technical/static/js/runtime~main.7bfc54a3.js"
+  },
+  {
+    "revision": "246e66cbacbbd516450e",
+    "url": "/front-end-technical/static/js/main.246e66cb.chunk.js"
+  },
+  {
+    "revision": "f3fe1bdee220da85c016",
+    "url": "/front-end-technical/static/js/1.f3fe1bde.chunk.js"
+  },
+  {
+    "revision": "246e66cbacbbd516450e",
+    "url": "/front-end-technical/static/css/main.bfc1dd70.chunk.css"
+  },
+  {
+    "revision": "1d9e9dbe389afc91e34b50c5fc6519c4",
+    "url": "/front-end-technical/index.html"
+  }
+];
